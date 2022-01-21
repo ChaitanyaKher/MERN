1ddf5223413104f56ac2c2367c86f0bb77062ca1
Kher <ckkher@147dda3bad26.ant.amazon.com>
2022-01-21 18:19:47 +0530

    Removing external js and writing in-line jsx

--- MyFirstComponent.js
+++
@@ -1,4 +0,0 @@
-const className = 'box';
-const element = React.createElement('div', {title: 'Outer div' },
-    React.createElement('h1', {className}, 'Hello World!'));
-ReactDOM.render(element, document.getElementById('contents'));
